155c1f502088ea373d09e7415498973ed1c8f306
ZacharyAdams <zaekadams@gmail.com>
2014-06-13 15:36:02 -0400

    users can now make questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,16 +5,33 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @questions = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def create
+    @question = Question.new(question_params)
 
+     if @question.save
+      redirect_to '/questions'
+    else
+      flash.now[:notice] = "Uh oh! Your question could not be saved."
+      render :new
+    end
   end
 
 
   def new
-
+    @question = Question.new
   end
 
 end
+
+
+
+  private
+
+  def question_params
+    # this method will return a hash like this:
+    # { title: "whatever title", author: "some person", body: "blah blah blah" }
+    params.require(:question).permit(:user_id, :title, :body)
+  end
