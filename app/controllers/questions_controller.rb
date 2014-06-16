996f6a3b1d106bb79cc6556be090d9810eab8e38
ZacharyAdams <zaekadams@gmail.com>
2014-06-16 13:48:50 -0400

    added some links so the user can navigate better around the site.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,6 +37,13 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+  @question = Question.find(params[:id])
+  @question.destroy
+
+  redirect_to questions_path
+  end
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers, only: [:create]
   end
+
+
+  root to: 'questions#index'
 end
